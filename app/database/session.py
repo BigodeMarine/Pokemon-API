e72ea620fc96b134e40634ab2cef144ff9f45e1a
Edson <cryrider8@gmail.com>
2026-08-18 19:25:55 -0300

    chore: remove database debug

--- app/database/session.py
+++ app/database/session.py
@@ -1,5 +1,4 @@
 from collections.abc import Generator
-from urllib.parse import urlparse
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 from app.core.config import settings
@@ -7,19 +6,6 @@ from app.core.config import settings
 """
 A Engine representa a conexão principal com o banco de dados.
 """
-
-db_url = urlparse(settings.DATABASE_URL)
-
-print(
-    "DATABASE DEBUG:",
-    {
-        "host": db_url.hostname,
-        "port": db_url.port,
-        "database": db_url.path,
-        "scheme": db_url.scheme,
-    },
-)
-
 engine = create_engine(
     settings.DATABASE_URL,
     echo=settings.DEBUG,
