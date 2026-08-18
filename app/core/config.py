76506a2db39a90c08478c7b8645d7d78bae985cf
Edson <cryrider8@gmail.com>
2026-08-18 18:06:24 -0300

    fix: config do redis para deploy

--- app/core/config.py
+++ app/core/config.py
@@ -16,9 +16,9 @@ class Settings(BaseSettings):
 
     DATABASE_URL: str
 
-    REDIS_HOST: str
-    REDIS_PORT: int
-    REDIS_DB: int
+    REDIS_HOST: str = "localhost"
+    REDIS_PORT: int = 6379
+    REDIS_DB: int = 0
     REDIS_PASSWORD: str = ""
     REDIS_URL: str = ""
 
